05cfda8c2fe890fa9ae5c8287431edfc8a98bf90
Luis David Gil Baez <luis_gil@ucne.edu.do>
2019-09-13 16:11:08 -0400

    agregando clases

---
+++ Tarea3/StructTienda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAP2.Tarea3
+{
+    class StructTienda
+    {
+    }
+}
